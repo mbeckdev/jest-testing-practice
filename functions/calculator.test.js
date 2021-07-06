c0e58e7a365fd322bf97808b0b9d08cdb821b340
mbeckdev <mbeckdev42@gmail.com>
2021-07-06 16:34:40 -0600

    Add calculator.add test for floats

--- functions/calculator.test.js
+++ functions/calculator.test.js
@@ -1,5 +1,10 @@
+import { expect } from '@jest/globals';
 import { calculator } from './calculator.js';
 
 test('adds 1 and 2, expects 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('adds 1 and 0.3, expects 1.3', () => {
+  expect(calculator.add(1, 0.3)).toBeCloseTo(1.3);
 });
