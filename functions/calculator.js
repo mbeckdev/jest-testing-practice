0fd19d9fda5874642c0301943c7600f4eb9f9461
mbeckdev <mbeckdev42@gmail.com>
2021-07-06 16:44:51 -0600

    Add calculator.subtract function and tests

--- functions/calculator.js
+++ functions/calculator.js
@@ -2,9 +2,12 @@ const calculator = (function () {
   function add(a, b) {
     return a + b;
   }
-
+  function subtract(b, a) {
+    return b - a;
+  }
   return {
     add,
+    subtract,
   };
 })();
 
--- functions/calculator.test.js
+++ functions/calculator.test.js
@@ -1,10 +1,20 @@
-import { expect } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import { calculator } from './calculator.js';
 
+// Tests for calculator.add
 test('adds 1 and 2, expects 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
 
-test('adds 1 and 0.3, expects 1.3', () => {
+test('float add check - adds 1 and 0.3, expects 1.3', () => {
   expect(calculator.add(1, 0.3)).toBeCloseTo(1.3);
+});
+
+// Tests for calculator.subtract
+test('subtracts 1 from 3, expects 2', () => {
+  expect(calculator.subtract(3, 1)).toBe(2);
+});
+
+test('float subtraction - subtracts 0.3 from 3, expects 2.7', () => {
+  expect(calculator.subtract(3, 0.3)).toBeCloseTo(2.7);
 });
